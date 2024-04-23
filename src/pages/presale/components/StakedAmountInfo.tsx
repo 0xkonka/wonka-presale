4cca797c2c6972a7513d6b755da8e706231fc91f
0xkonka <8722069+0xkonka@users.noreply.github.com>
2024-04-23 13:11:59 -0500

    fix mapping error

--- src/pages/presale/components/StakedAmountInfo.tsx
+++ src/pages/presale/components/StakedAmountInfo.tsx
@@ -32,9 +32,8 @@ const StakedAmountInfo = () => {
         </Typography>
 
         {totalContributedPerChain.map((id, index) => (
-          <>
+          <Box key={index}>
             <Typography
-              key={index}
               variant={isSmallScreen ? 'subtitle1' : 'h5'}
               color='#F3F3F3'
               sx={{
@@ -71,7 +70,7 @@ const StakedAmountInfo = () => {
                 { value: +formatUnits(id.capPerLevel, 6), label: `$${+formatUnits(id.capPerLevel, 6)}` }
               ]}
             />
-          </>
+          </Box>
         ))}
       </Stack>
     </Card>
